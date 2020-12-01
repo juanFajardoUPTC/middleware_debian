ffa7e50643de747b5265dce32ccf37be8aef8912
juanFajardoUPTC <juanFajardoUPTC@github.com>
2020-12-01 11:48:01 -0500

    arreglo bash

--- bash.sh
+++ bash.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+node api.js &
 x=1
+echo 
 while [ $x -le 5000 ]
 do
-    result=`ping -c 1 192.168.0.13 | xargs -n1 -i bash -c 'echo `date +%F\ %T`" {}"'`
+    result=`ping -c 1 192.168.0.13 | while read pong; do echo "$(date): $pong"; done`
 	echo $result >> "servidor1.txt"
 
-    result=`ping -c 1 192.168.0.22 | xargs -n1 -i bash -c 'echo `date +%F\ %T`" {}"'`
+    result=`ping -c 1 192.168.0.22 | while read pong; do echo "$(date): $pong"; done`
 	echo $result >> "servidor2.txt"
 
     sleep 1
